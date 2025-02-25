0bad8cafdea72b0504d634eea7427cdfaf8f13e7
zntwpp <yarikshcher@gmail.com>
2025-02-25 21:58:39 +0300

    added substraction operation

--- calc.asm
+++ calc.asm
@@ -3,10 +3,13 @@ section .data
 	frst_len: equ $-frst_msg
 	scnd_msg: db 'enter second num', 10
 	scnd_len: equ $-scnd_msg 
+	thrd_msg: db 'plus or minus, 1 or 2', 10
+	thrd_len: equ $-thrd_msg
 
 section .bss
 	first_num resb 2
 	second_num resb 2
+	sign resb 1
 	res resb 1
 
 section .text
@@ -37,7 +40,25 @@ _start:
 	mov ecx, second_num
 	mov edx, 2
 	int 80h
+	;sign
+	mov eax,4
+	mov ebx,1
+	mov ecx, thrd_msg
+	mov edx, thrd_len
+	int 80h
+	;input
+	mov eax, 3
+	mov ebx, 2
+	mov ecx, sign
+	mov edx, 1
+	int 80h
 	;print res
+	mov edx, [sign]
+	cmp edx, '1'
+	je plus
+	cmp edx, '2'
+	je minus
+plus:	
 	mov eax, [first_num]
 	sub eax, '0'
 	
@@ -49,7 +70,23 @@ _start:
 	
 	
 	mov[res], eax
+	jmp print
 	
+minus:
+	mov eax, [first_num]
+	sub eax, '0'
+
+	mov ebx, [second_num]
+	sub ebx, '0'
+
+	sub eax, ebx
+	add eax, '0'
+
+	mov[res], eax
+
+	jmp print
+
+print:	
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, res
